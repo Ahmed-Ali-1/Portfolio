8be18cf241f0312da5d86b4979d5b9899269637e
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-07-08 03:00:03 +0500

    set cross icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,15 @@
-var menubtn = document.getElementById("menubtn")
-var sideNav = document.getElementById("sideNav")
-var menu = document.getElementById("menu")
+var menubtn = document.getElementById("menubtn");
+var sideNav = document.getElementById("sideNav");
+var menu = document.getElementById("menu");
 
 sideNav.style.top = "-280px"; // initially hidden
 
-menubtn.onclick = function(){
-    if(sideNav.style.top == "-280px"){
-        sideNav.style.top = "0";
-        menu.src = "cross.png";
-    }
-    else{
-        sideNav.style.top = "-280px";
-        menu.src = "menu.png";
-    }
-}
+menubtn.onclick = function () {
+  if (sideNav.style.top == "-280px") {
+    sideNav.style.top = "0";
+    menu.src = "assets/images/cross.png";
+  } else {
+    sideNav.style.top = "-280px";
+    menu.src = "assets/images/cross.png";
+  }
+};
