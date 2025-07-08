69ac635269c84d0e0c0ed29a0e7ccee7b3afb6e6
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-07-08 14:46:06 +0500

    Set MenuBar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,14 @@ var menubtn = document.getElementById("menubtn");
 var sideNav = document.getElementById("sideNav");
 var menu = document.getElementById("menu");
 
-sideNav.style.top = "-280px"; // initially hidden
+sideNav.style.top = "-280px";
 
 menubtn.onclick = function () {
   if (sideNav.style.top == "-280px") {
     sideNav.style.top = "0";
-    menu.src = "assets/images/cross.png";
+    menu.src = "assets/Images/cross.png";
   } else {
     sideNav.style.top = "-280px";
-    menu.src = "assets/images/cross.png";
+    menu.src = "assets/Images/menu.png";
   }
 };
